b6fbdaa8a24cf5f6287bd0edff22e776a19177aa
kirill pavlov <2020kirillp@gmail.com>
2024-07-03 12:29:57 +0300

    Refactor fetchRandomWord function and update userSetLength state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,24 @@ type Word = {
 	word: string;
 };
 
-const randomQuote = (): Word => {
-	// return quotes[Math.floor(quotes.length * Math.random())];
-
-	useEffect(() => {
-		fetch(
-			"https://api.api-ninjas.com/v1/randomword".then(
-				(response) => response.json
-			)
-		);
-	});
-};
-
 export default function Home() {
 	const [quote, setQuote] = useState<Quote>();
 	const [text, setText] = useState<string>("");
 	const [currentWord, setCurrentWord] = useState<string>();
 	const quotesSplit = useMemo(() => quote?.quote.split(" ") ?? [], [quote]);
 	const [wordIdx, setWordIdx] = useState<number>(0);
+
 	const [userSetLength, setUserSetLength] = useState<number>(20);
 
+	const fetchRandomWord = (): Word => {
+		// return quotes[Math.floor(quotes.length * Math.random())];
+		fetch(`https://random-word-api.herokuapp.com/word?number=${userSetLength}`)
+			.then((response) => response.json)
+			.catch((error) => console.error(error));
+	};
+
 	useEffect(() => {
-		const fetchQuote = async () => {
-			const quote = await randomQuote();
-			setQuote(quote);
-		};
-		fetchQuote();
+		fetchRandomWord();
 	}, []);
 
 	useEffect(() => {
@@ -70,13 +62,13 @@ export default function Home() {
 					<span className="text-black">{quote?.quote}</span>
 				</p>
 				<div>
-					<button>15</button>
+					<button onClick={() => setUserSetLength(15)}>15</button>
 					<span>/</span>
-					<button>20</button>
+					<button onClick={() => setUserSetLength(20)}>20</button>
 					<span>/</span>
-					<button>25</button>
+					<button onClick={() => setUserSetLength(25)}>25</button>
 					<span>/</span>
-					<button>50</button>
+					<button onClick={() => setUserSetLength(50)}>50</button>
 				</div>
 			</div>
 			<input
